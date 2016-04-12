cb36860b5003e7388f419c3855d41eb3f29fcfac
Damian Gryski <dgryski@gmail.com>
2016-04-12 16:37:13 +0200

    reuse bigints during trial division (if possible)

--- trial.go
+++ trial.go
@@ -9,16 +9,20 @@ func Trial(n *big.Int) (primes []*big.Int, composites []*big.Int) {
 	var z big.Int
 	var m big.Int
 
+	bigp := big.NewInt(0)
 
 	for _, p := range smallPrimes {
 		for {
-			bigp := big.NewInt(p)
+			// reuse the existing bigint
+			bigp.SetInt64(p)
 			z.DivMod(n, bigp, &m)
 			if m.Sign() != 0 {
 				break
 			}
-			primes = append(primes, bigp)
 			n.Set(&z)
+			primes = append(primes, bigp)
+			// create a new bigint, since we just stored the one we were using
+			bigp = big.NewInt(0)
 		}
 
 		if n.Cmp(one) == 0 {
